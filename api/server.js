41a55d68e1232686a985b6ef1e80608fc95629d5
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2016-04-23 20:46:01 +0200

    chore: basic socket mysql sequelize functionallity

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
-server.listen(8080);
+server.listen(8080, function(){
+	console.log("server listening to 8080")
+});
+
+
+
 var mysql = require('mysql');
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('gmbh', 'root', '',
@@ -9,7 +14,9 @@ var sequelize = new Sequelize('gmbh', 'root', '',
   port: 3306
 });
 
+
 io.on('connection', function(socket){
+	console.log("someone connected")
   socket.on('event', function(data){
 
   })
