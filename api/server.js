0624080728c979114adf0f00a01380d1b367299d
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2016-04-23 20:02:00 +0200

    chore: delete feathers -> start pure node.js with socket.io

---
+++ api/server.js
@@ -0,0 +1,29 @@
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+server.listen(8080);
+var mysql = require('mysql');
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('gmbh', 'root', '',
+{
+  host: "localhost",
+  port: 3306
+});
+
+io.on('connection', function(socket){
+  socket.on('event', function(data){
+
+  })
+
+
+
+
+
+  socket.on('disconnect', function(){});
+});
+
+
+
+
+function foo() {
+  		
+  	}
