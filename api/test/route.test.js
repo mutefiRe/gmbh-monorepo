907d03241ac11560c3f425e7182f4819d6d38489
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2016-04-24 23:12:04 +0200

    test: route testing
    test user route

---
+++ api/test/route.test.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const chai = require('chai');
+const mocha = require('mocha');
+const request = require('request');
+const should = chai.should();
+const Route = require('../router/user');
+
+describe('user route', () => {
+	it('should response', () => {
+	 	 request.get('http://localhost:8080/user/10', function(err, res, body){
+	 	 	expect(res.statusCode).to.equal(200);
+	 	 	expect(res.body).to.equal('user 10');
+	 	 	done();
+	 	 });
+    });
+});
