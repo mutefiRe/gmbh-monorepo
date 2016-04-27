1b5b52bc84834b8c0dcafadba7de45395d701e14
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2016-04-27 20:53:00 +0200

    feat: add response to create route, made users and user query

--- api/test/main.test.js
+++ api/test/main.test.js
@@ -3,15 +3,6 @@ const routeTest = require("./route.test")
 const userTest = require("./user.test")
 const authenticateTest = require("./authenticate.test")
 
-    db.User.create({
-      username: "testUser",
-      firstname: "max",
-      lastname: "mustermann",
-      password: "testPW",
-      permission: 1,
-      token: "abc123"
-    })
-
 serverTest();
 routeTest();
 userTest();
