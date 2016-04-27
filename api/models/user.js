f66b6e2525074b9ba70fac8491d5b5f7640d22ab
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2016-04-27 16:24:57 +0200

    refactor: new style of model building

--- api/models/user.js
+++ api/models/user.js
@@ -1,16 +1,37 @@
 "use strict";
 
-const DB = require("../config/DBconfig");
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(DB().database, DB().user, DB().password, DB().host);
+module.exports = function(sequelize, DataTypes) {
+  let User = sequelize.define("User", {
+    username: {type: DataTypes.STRING, allowNull: false,  unique: true},
+    firstname: {type: DataTypes.STRING, allowNull: true,  unique: false},
+    lastname: {type: DataTypes.STRING, allowNull: true,  unique: false},
+    password: {type: DataTypes.STRING, allowNull: false,  unique: false},
+    permission: {type: DataTypes.INTEGER, allowNull:false, unique: false}
+  }/*, {
+    classMethods: {
+      associate: function(models) {
+        User.hasMany(models.Task)
+      }
+    }
+  }*/);
+  User.sync();
 
+  return User;
+};
+/*
+"use strict";
+
+
+const DataTypes = require('sequelize');
+const DB = require("../config/DBconfig");
+const sequelize = new DataTypes(DB().database, DB().user, DB().password, DB().host);
 
 let User = sequelize.define('user', {
-  username: {type: Sequelize.STRING, allowNull: false,  unique: true},
-  firstname: {type: Sequelize.STRING, allowNull: true,  unique: false},
-  lastname: {type: Sequelize.STRING, allowNull: true,  unique: false},
-  password: {type: Sequelize.STRING, allowNull: false,  unique: false},
-  permission: {type: Sequelize.INTEGER, allowNull:false, unique: false}
+  username: {type: DataTypes.STRING, allowNull: false,  unique: true},
+  firstname: {type: DataTypes.STRING, allowNull: true,  unique: false},
+  lastname: {type: DataTypes.STRING, allowNull: true,  unique: false},
+  password: {type: DataTypes.STRING, allowNull: false,  unique: false},
+  permission: {type: DataTypes.INTEGER, allowNull:false, unique: false}
 })
 
 User.sync();
@@ -32,3 +53,4 @@ module.exports = {
 	create: create,
 	User: User
 }
+*/
