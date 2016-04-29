95a7e83de107b354d01afe5a2149012354ad4b01
Konrad Kleeberger <git@konrad.online>
2016-04-29 23:40:09 +0200

    feat: login with JWT
    login form with error handling

---
+++ order/app/adapters/application.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+
+export default DS.RESTAdapter.extend({
+  host: 'http://localhost:8080',
+  namespace: 'api'
+});
+
+// export default DS.RESTAdapter.extend(DataAdapterMixin, {
+//   authorizer: 'authorizer:application'
+// });
--- order/app/routes/login.js
+++ order/app/routes/login.js
@@ -1,14 +1,4 @@
 import Ember from 'ember';
 
-var user = {
-  id: 1,
-  firstName: "Thomas",
-  lastName: "Stütz",
-  password: ""
-}
-
 export default Ember.Route.extend({
-  model(){
-    return user
-  }
 });
