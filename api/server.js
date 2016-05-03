325eb2ac2f41ba4bfc16dfc81a89136c634ff64d
Konrad Kleeberger <git@konrad.online>
2016-05-03 15:37:28 +0200

    fix: CROS Headers for login with JWT token over x-access-token header field

--- api/server.js
+++ api/server.js
@@ -29,7 +29,7 @@ app.all('*', function(req, res, next){
 });
 app.options('*', function(req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, x-access-token');
     res.send(200)
 });
 app.use('/authenticate', authenticate);
