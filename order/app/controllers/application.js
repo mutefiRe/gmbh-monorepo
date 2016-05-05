b92a21b46a7ed77b7595f99eb5d0e63d61697777
Konrad Kleeberger <git@konrad.online>
2016-05-05 13:22:09 +0200

    feat: global currentUser variable for views
    accesses user id from the payload of the auth token and starts a request to get user information
    from the backend and stored response into the variable

--- order/app/controllers/application.js
+++ order/app/controllers/application.js
@@ -1,7 +1,20 @@
 import Ember from 'ember';
+import getOwner from 'ember-getowner-polyfill';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service("session"),
+  currentUser: Ember.computed('session.session.content', function() {
+    const authenticator = getOwner(this).lookup('authenticator:jwt'),
+        session = this.get('session.session.content'),
+	token = session.authenticated.token;
+    let tokenData = {}; 
+	if(token) {
+		tokenData = authenticator.getTokenData(token);
+		return this.store.findRecord('user', tokenData.id);
+	} else {
+		return false;
+	}
+  }),
   actions: {
     logout() {
       this.get("session").invalidate();
