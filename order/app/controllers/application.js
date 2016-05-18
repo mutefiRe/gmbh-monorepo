91a994548593c4ee6b3fd1b09c9a5ecbde7002ad
Konrad Kleeberger <git@konrad.online>
2016-05-18 18:52:54 +0200

    fix: fixed missing edge case for session

--- order/app/controllers/application.js
+++ order/app/controllers/application.js
@@ -5,15 +5,17 @@ export default Ember.Controller.extend({
   session: Ember.inject.service("session"),
   currentUser: Ember.computed('session.session.content', function() {
     const authenticator = getOwner(this).lookup('authenticator:jwt'),
-        session = this.get('session.session.content'),
-	token = session.authenticated.token;
-    let tokenData = {}; 
-	if(token) {
-		tokenData = authenticator.getTokenData(token);
-		return this.store.findRecord('user', tokenData.id);
-	} else {
-		return false;
-	}
+        session = this.get('session.session.content');
+    if(session && session.authenticated.token) {
+      const tokenData = authenticator.getTokenData(session.authenticated.token);
+      if(tokenData.id) {
+        return this.store.findRecord('user', tokenData.id);
+      } else {
+        return false;
+      }
+    } else {
+  		return false;
+  	}
   }),
   actions: {
     logout() {
--- order/tests/unit/controllers/application-test.js
+++ order/tests/unit/controllers/application-test.js
@@ -1,12 +1,26 @@
-import { moduleFor, test } from 'ember-qunit';
+import { expect } from 'chai';
+import { describeModule, it } from 'ember-mocha';
+//import login from 'gmbh/tests/helpers/login'
+import moduleForAcceptance from 'gmbh/tests/helpers/module-for-acceptance';
+import { authenticateSession } from 'gmbh/tests/helpers/ember-simple-auth';
 
-moduleFor('controller:application', 'Unit | Controller | application', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
-});
+describeModule(
+  'controller:application',
+  'ApplicationController',
+  {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+    //  needs: ['application']
+  },
+  function() {
+    it('exists', function() {
+      let controller = this.subject();
+      expect(controller).to.be.ok;
+    });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let controller = this.subject();
-  assert.ok(controller);
-});
+    it('no session is set', function() {
+      let controller = this.subject();
+      expect(controller.get('currentUser')).to.be.false
+    });
+  }
+);
