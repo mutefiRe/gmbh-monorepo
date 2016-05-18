7f9646cb0cb057616eaf1d5ca39ac8354f387524
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2016-05-18 18:27:06 +0200

    test(api restricted path): add testcases for token expired, no token provided

---
+++ api/test/api.test.js
@@ -0,0 +1,91 @@
+'use strict'
+
+module.exports  = function(){
+
+  var mocha = require('mocha');
+  var chai = require('chai');
+  var should = chai.should();
+  var app = require('../server.js');
+  var db = require('../models/index');
+  var chaiHttp = require('chai-http');
+  const jwt    = require('jsonwebtoken');
+  const config = require('../config/config.js');
+  chai.use(chaiHttp);
+
+  function before(){
+    db.User.sync({force:true}).then(() => {
+     db.User.create({
+      username: "test",
+      firstname: "test",
+      lastname: "test",
+      password: "test",
+      permission: 1
+    })
+   })
+  }
+
+  describe('/api route -> check restriction access', () => {
+
+    var token = jwt.sign({
+      username: "test",
+      firstname: "test",
+      lastname: "test",
+      permission: 1
+    }, config.secret, { expiresIn: '24h' });
+
+    it('should response status 200 to api call, when send with token', (done) => {
+      chai.request(app)
+      .get('/api/')
+      .send({ token: token })
+      .then( res => {
+        res.status.should.be.equal(200)
+        res.body.msg.should.be.equal("you have access to the api")
+        done();
+      })
+      .catch( res => {
+      })
+    })
+
+
+    it('should response status 400 to api call, when send with wrong token', (done) => {
+      chai.request(app)
+      .get('/api/')
+      .send({ token: token+1 })
+      .catch( res => {
+        res.status.should.be.equal(400)
+        res.response.body.error.msg.should.be.equal("invalid signature")
+        done();
+      })
+    })
+
+
+    it('should response status 400 to api call, when send without token', (done) => {
+      chai.request(app)
+      .get('/api/')
+      .send({})
+      .catch( res => {
+        res.status.should.be.equal(400)
+        res.response.body.error.msg.should.be.equal("No token provided")
+        done();
+      })
+    })
+
+    it('should response status 400 to api call, when send with expired token', (done) => {
+      var expiredToken =  jwt.sign({
+        username: "test",
+        firstname: "test",
+        lastname: "test",
+        permission: 1
+      }, config.secret, { expiresIn: '0' });
+
+      chai.request(app)
+      .get('/api/')
+      .send({ token: expiredToken })
+      .catch( res => {
+       res.status.should.be.equal(400)
+       res.response.body.error.msg.should.be.equal("jwt expired")
+       done();
+     })
+    })
+  })
+}
--- api/test/main.test.js
+++ api/test/main.test.js
@@ -3,7 +3,10 @@
 const serverTest = require("./server.test")
 const routeTest = require("./route.test")
 const userTest = require("./user.test")
+const apiTest = require("./api.test")
 const authenticateTest = require("./authenticate.test")
 const db = require('../models/index');
 
 authenticateTest();
+userTest();
+apiTest();
