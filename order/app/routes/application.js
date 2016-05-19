97748912291ed4b798241ceffd06d9fa2f1bd9fb
Josef Krabath <jkrabath.mmt-b2014@fh-salzburg.ac.at>
2016-05-19 21:47:16 +0200

    feat: implemented dynamic redirect after login (cases: admin, waiter)

--- order/app/routes/application.js
+++ order/app/routes/application.js
@@ -1,5 +1,26 @@
 import Ember from 'ember';
+import getOwner from 'ember-getowner-polyfill';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  sessionAuthenticated() {
+    const session = this.get('session.session.content');
 
+    if (session.authenticated.token) {
+      const authenticator = getOwner(this).lookup('authenticator:jwt');
+      const userPermission = authenticator.getTokenData(session.authenticated.token).permission;
+
+      switch (userPermission) {
+        case 0:
+          this.transitionTo('/dashboard');
+          break;
+        case 1:
+          this.transitionTo('/order');
+          break;
+        default:
+          this.transitionTo('/login');
+          break;
+      }
+    }
+  }
+});
