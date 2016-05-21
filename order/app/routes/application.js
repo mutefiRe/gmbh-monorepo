7972c7180529ea945b7b8e2592a8cf1c26e2ec59
Konrad Kleeberger <git@konrad.online>
2016-05-21 15:51:03 +0200

    feat: websocket connection with backend
    enables for logged in users a websocket connection with the backend. The backend can send data to
    the update route. This data gets pushed into the Ember Store.

--- order/app/routes/application.js
+++ order/app/routes/application.js
@@ -1,5 +1,38 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+function updateData(io, token, store) {
+  const socket = io.createSocket(window.EmberENV.host, {
+    query: `token=${token}`
+  });
 
+  socket.on('update', function (payload) {
+    store.pushPayload(JSON.parse(payload));
+  });
+}
+
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  socketService: Ember.inject.service('socket-io'),
+  session: Ember.inject.service('session'),
+  store: this.store,
+  init() {
+    const session = this.get('session');
+    const io = this.get('socketService');
+    const store = this.get('store');
+
+    session.on('authenticationSucceeded', function () {
+      const token = this.get('session.content').authenticated.token;
+
+      updateData(io, token, store);
+    });
+
+  },
+  afterModel() {
+    const io = this.get('socketService');
+    const content = this.get('session.session.content');
+
+    if (this.get('session.isAuthenticated')) {
+      updateData(io, content.authenticated.token, this.store);
+    }
+  }
+});
