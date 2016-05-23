4559ca2b10f110d6021ff612c97d6a68eb15748a
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2016-05-23 19:18:59 +0200

    feat: added color and its regex validation to category

--- api/models/category.js
+++ api/models/category.js
@@ -5,6 +5,14 @@ module.exports = function(sequelize, DataTypes) {
     name: {type: DataTypes.STRING, allowNull: false,  unique: false},
     enabled: {type: DataTypes.BOOLEAN, allowNull: false,  unique: false},
     description: {type: DataTypes.STRING, allowNull: false, unique: false},
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+      validate: {
+        is: /^(#[a-f0-9]{6}|#[a-f0-9]{3}|rgb *\( *[0-9]{1,3}%? *, *[0-9]{1,3}%? *, *[0-9]{1,3}%? *\)|rgba *\( *[0-9]{1,3}%? *, *[0-9]{1,3}%? *, *[0-9]{1,3}%? *, *[0-9]{1,3}%? *\)|black|green|silver|gray|olive|white|yellow|maroon|navy|red|blue|purple|teal|fuchsia|aqua)$/i
+      }
+    },
   }, {
     classMethods: {
       associate: function(models) {
