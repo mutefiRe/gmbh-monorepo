16676bade636c5b0a27aaa87f15d1137daeb923a
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2016-05-23 23:34:28 +0200

    fix: changed color allowNull to true

--- api/models/category.js
+++ api/models/category.js
@@ -7,7 +7,7 @@ module.exports = function(sequelize, DataTypes) {
     description: {type: DataTypes.STRING, allowNull: false, unique: false},
     color: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
       unique: false,
       validate: {
         is: /^(#[a-f0-9]{6}|#[a-f0-9]{3}|rgb *\( *[0-9]{1,3}%? *, *[0-9]{1,3}%? *, *[0-9]{1,3}%? *\)|rgba *\( *[0-9]{1,3}%? *, *[0-9]{1,3}%? *, *[0-9]{1,3}%? *, *[0-9]{1,3}%? *\)|black|green|silver|gray|olive|white|yellow|maroon|navy|red|blue|purple|teal|fuchsia|aqua)$/i
