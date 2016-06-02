473ec04d5c543690ab3952a759e60480faa691f6
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2016-06-02 21:55:19 +0200

    feat: bind color-set class to items with mixin

--- order/app/components/single-item.js
+++ order/app/components/single-item.js
@@ -3,6 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'button',
   classNames: ['single-item'],
+  classNameBindings: ['myStyle'],
+  myStyle: 'style-1',
+  init() {
+    this._super();
+    const numberOfStyles = 4;
+
+    this.set('myStyle', `style-${(this.get('item').get('category').get('id') % numberOfStyles)}`);
+  },
   actions: {
   },
   click() {
