178d4033de214bfaeb440ec95629abbfe338440a
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2016-06-02 20:21:43 +0200

    feat: update table selection

--- order/app/components/table-item.js
+++ order/app/components/table-item.js
@@ -2,5 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'button',
-  classNames: ['table-item', 'button-3']
+  classNames: ['table-item'],
+  classNameBindings: ['priority'],
+  priority: 'style-1',
+  init() {
+    this._super();
+    const numberOfStyles = 4;
+
+    this.set('priority', `style-${(this.get('table').get('area').get('id') % numberOfStyles)}`);
+  }
 });
