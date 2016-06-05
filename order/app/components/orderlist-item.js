004faf0fa627ef1799eaf88e2cabce87e2cf93da
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2016-06-05 15:42:36 +0200

    feat: added functionality for deleting single orderitems

--- order/app/components/orderlist-item.js
+++ order/app/components/orderlist-item.js
@@ -3,9 +3,21 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['orderlist-item'],
   tagName: 'tr',
+  isTabbed: false,
+  click(){
+    if(this.get('isTabbed')){
+      this.set('isTabbed',false);
+    }
+    else{
+      this.set('isTabbed',true);
+    }
+  },
   actions: {
     deleteOrderItem() {
       this.get('deleteOrderItem')(this.get('index'));
+    },
+    removeItemFromOrder(){
+      this.get('removeItemFromOrder')(this.get('data'));
     }
   }
 });
