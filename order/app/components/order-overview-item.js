21b8dc00bf4dbf53e8234e6a7d69977b8b84c7c3
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2016-06-06 17:04:10 +0200

    feat: update order-overview
    remaining: sum of price, return button, action on item click

--- order/app/components/order-overview-item.js
+++ order/app/components/order-overview-item.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'tr'
+  classNames: ['order-overview-item'],
+  tagName: 'tr',
+  classNameBindings: ['bezahlt'],
+  bezahlt: function() {
+    if (this.get('status') === 'bezahlt') {
+      return 'bezahlt';
+    }
+    return 'offen';
+  }.property('status')
 });
---
+++ order/tests/unit/helpers/show-time-test.js
@@ -0,0 +1,17 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import {
+  describe,
+  it
+} from 'mocha';
+import {
+  showTime
+} from 'gmbh/helpers/show-time';
+
+describe('ShowTimeHelper', function() {
+  // Replace this with your real tests.
+  it('works', function() {
+    let result = showTime(42);
+    expect(result).to.be.ok;
+  });
+});
