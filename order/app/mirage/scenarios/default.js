fd5b4e7ef179185ddfe32ccf02c0598fcc78c6df
Josef Krabath <jkrabath.mmt-b2014@fh-salzburg.ac.at>
2016-06-06 21:40:24 +0200

    refactor: switched to version 0.2
    folder /app/mirage moved to /mirage

--- order/app/mirage/scenarios/default.js
+++
@@ -1,9 +0,0 @@
-export default function (server) {
-  server.createList('user', 10);
-  server.createList('category', 5);
-  server.createList('item', 20);
-  server.createList('unit', 20);
-  server.createList('order', 10);
-  server.createList('table', 5);
-  server.createList('orderitem', 20);
-}
