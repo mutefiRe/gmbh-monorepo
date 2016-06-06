863a5c0d78f98bac9e5c2a066a795081e53684fd
Josef Krabath <jkrabath.mmt-b2014@fh-salzburg.ac.at>
2016-06-06 11:44:54 +0200

    feat: added authenticated-route-mixin to all routes

--- order/app/routes/dashboard.js
+++ order/app/routes/dashboard.js
@@ -1,4 +1,4 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from '../mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
-});
+export default Ember.Route.extend(AuthenticatedRouteMixin);
