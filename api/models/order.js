3314d6c7a465114b412256b0323fd92be42eaf19
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2016-06-06 18:05:26 +0200

    feat: added isPaid to Order

--- api/models/order.js
+++ api/models/order.js
@@ -2,7 +2,8 @@
 
 module.exports = function(sequelize, DataTypes) {
   let Order = sequelize.define("Order", {
-
+    totalAmount: {type: DataTypes.INTEGER, allowNull: false,  unique: false},
+    isPaid: {type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false,  unique: false}
   }, {
     classMethods: {
       associate: function(models) {
--- api/serializers/order.js
+++ api/serializers/order.js
@@ -2,6 +2,8 @@
 
 module.exports = function(data){
   let order = {
+    totalAmount: data.totalAmount,
+    isPaid: data.isPaid || false,
     UserId: data.user,
     TableId: data.table
   }
