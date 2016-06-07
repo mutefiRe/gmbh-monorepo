8f90096d8a08fccdaa5516e2edde5a29c399a13f
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2016-06-07 23:45:41 +0200

    feat: added payview

--- order/app/components/order-detail-view-item.js
+++ order/app/components/order-detail-view-item.js
@@ -1,6 +1,55 @@
 import Ember from 'ember';
+import _ from 'lodash/lodash';
 
 export default Ember.Component.extend({
   classNames: ['order-detail-view-item'],
-  tagName: 'tr'
+  classNameBindings: ['css'],
+  tagName: 'tr',
+  click(){
+    let viewOrder;
+    let payOrder;
+
+    if(this.get('pay') === "false"){
+      viewOrder = _.cloneDeep(this.get('viewOrder'));
+      payOrder = _.cloneDeep(this.get('payOrder'));
+    }else{
+      payOrder = _.cloneDeep(this.get('viewOrder'));
+      viewOrder = _.cloneDeep(this.get('payOrder'));
+    }
+    if(viewOrder.items[this.get('orderItem.identifier')].isPaid === false){
+      let orderItem = this.get('orderItem');
+      if(viewOrder.items[orderItem.identifier] != undefined){
+        let decrease = viewOrder.items[orderItem.identifier].prize / viewOrder.items[orderItem.identifier].amount;
+        viewOrder.totalAmount -= decrease;
+        viewOrder.items[orderItem.identifier].prize -= decrease;
+        payOrder.totalAmount += decrease;
+        if(payOrder.items[orderItem.identifier] != undefined){
+          payOrder.items[orderItem.identifier].amount++;
+          viewOrder.items[orderItem.identifier].amount--;
+          payOrder.items[orderItem.identifier].prize += decrease;
+        }
+        else{
+          payOrder.items[orderItem.identifier] = {}
+          payOrder.items[orderItem.identifier] = _.cloneDeep(viewOrder.items[orderItem.identifier]);
+          payOrder.items[orderItem.identifier].amount = 1;
+          viewOrder.items[orderItem.identifier].amount--;
+          payOrder.items[orderItem.identifier].prize = decrease;
+        }
+        payOrder.items[orderItem.identifier].isPaid = viewOrder.items[orderItem.identifier].isPaid;
+
+        if(viewOrder.items[orderItem.identifier].amount === 0){
+          delete(viewOrder.items[orderItem.identifier]);
+        }
+        if(this.get('pay') === "false"){
+          this.set('viewOrder', viewOrder);
+          this.set('payOrder', payOrder);
+        }else{
+          this.set('viewOrder', payOrder);
+          this.set('payOrder',  viewOrder);
+        }
+      }
+    }else{
+      this.set('paid', 'herbert');
+    }
+  }
 });
--- order/app/components/order-detail-view.js
+++ order/app/components/order-detail-view.js
@@ -1,14 +1,17 @@
 import RecognizerMixin from 'ember-gestures/mixins/recognizers';
 import Ember from 'ember';
+import _ from 'lodash/lodash';
 
 const {
   Component
 } = Ember;
 
 export default Ember.Component.extend(RecognizerMixin, {
-  recognizers: 'swipe',
   classNames: ['order-detail-view'],
+  recognizers: 'swipe',
   classNameBindings: ['SwipeChange'],
+  viewOrder : {items: {}, totalAmount: 0},
+  payOrder: {items: {}, totalAmount: 0},
   SwipeChange: function () {
     if (this.get('swipeHelper.order-detail.active') && this.get('swipeHelper.order-overview.last')) {
       return 'slide-left-in';
@@ -18,6 +21,38 @@ export default Ember.Component.extend(RecognizerMixin, {
 
     return 'none';
   }.property('swipeHelper.order-detail.active'),
+
+  observedOrder: function () {
+    let order = this.get('order');
+    let viewOrder = {items: {}, totalAmount: 0};
+    let payOrder = {items: {}, totalAmount: 0};
+    order.get('orderitems').forEach((orderitem)=> {
+      let item = orderitem.get('item');
+      let id = item.get('id');
+      let extras = orderitem.get('extras');
+      let isPaid = orderitem.get('isPaid');
+      if (viewOrder.items[id+extras+isPaid] === undefined) {
+        viewOrder.items[id+extras+isPaid] = {};
+        viewOrder.items[id+extras+isPaid].amount = 0;
+      }
+      viewOrder.items[id+extras+isPaid].identifier = id+extras+isPaid;
+      viewOrder.items[id+extras+isPaid].amount++;
+      if(!orderitem.get('isPaid')){
+        viewOrder.totalAmount += (item.get('price'));
+
+      }
+      viewOrder.items[id+extras+isPaid].isPaid = orderitem.get('isPaid');
+      viewOrder.items[id+extras+isPaid].prize = (item.get('price') * viewOrder.items[id+extras+isPaid].amount).toFixed(2);
+      viewOrder.items[id+extras+isPaid].categoryId = item.get('category.id');
+      viewOrder.items[id+extras+isPaid].name = item.get('name') + " " + item.get('amount') + item.get('unit.name');
+      viewOrder.items[id+extras+isPaid].extras = extras || null;
+      viewOrder.items[id+extras+isPaid].id = id;
+
+      this.set('viewOrder', viewOrder);
+    })
+    this.set('payOrder', payOrder);
+  }.observes('order'),
+
   swipeRight() {
     this.triggerAction({
       action: 'goToOrderOverview',
@@ -30,6 +65,26 @@ export default Ember.Component.extend(RecognizerMixin, {
       this.set('swipeHelper.order-overview.last', false);
       this.set('swipeHelper.order-detail.active', false);
       this.set('swipeHelper.order-detail.last', true);
+    },
+    paySelected(){
+      let items = this.get('payOrder.items');
+      let orderitems = this.get('order.orderitems');
+      for(let Item in items){
+        let amount = items[Item].amount;
+        orderitems.forEach((item)=>{
+          let extras = item.get('extras');
+          let id = item.get('item.id');
+          let isPaid = item.get('isPaid');
+          if(amount > 0){
+            if(items[Item].identifier == id+extras+isPaid){
+              amount--;
+              item.set("isPaid", true);
+              console.log(item.toJSON());
+              item.save().then((item) => console.log("HALLO"));
+            }
+          }
+        })
+      }
     }
   }
 });
