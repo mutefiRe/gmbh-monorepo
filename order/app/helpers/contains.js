df8c881b658640f35ce7e36701c17bb97f516e52
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2016-07-01 18:08:26 +0200

    chore: removed table filtering again because its unintuitive with android keyboard

--- order/app/helpers/contains.js
+++ order/app/helpers/contains.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export function contains(params/* , hash */) {
-    if (params[0].toLowerCase().includes(params[1].toLowerCase()))
+   // if (params[0].toLowerCase().includes(params[1].toLowerCase()))
+    if (params[0].toLowerCase().indexOf(params[1].toLowerCase())==0)
         return true;
     else return false;
 }
