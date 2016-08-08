5d420db3d6ea40c40d5ba998c09c4875245e6442
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2016-08-08 23:00:33 +0200

    feat: fixes #29 - added ember-sortable, made items sortable by drag and drop, still needs rework in design for sure

--- order/app/models/item.js
+++ order/app/models/item.js
@@ -1,10 +1,12 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   amount: DS.attr('number'),
   price: DS.attr('number'),
   tax: DS.attr('number'),
+  sortId: DS.attr('number'),
   unit: DS.belongsTo('unit'),
   category: DS.belongsTo('category')
 });
