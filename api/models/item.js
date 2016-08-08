ef4c4c46fa1f6e635269163204030ae10a96a44f
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2016-08-09 00:11:13 +0200

    fix: minor change for sortable, make sortId not unique to prevent validation errors

--- api/models/item.js
+++ api/models/item.js
@@ -6,7 +6,7 @@ module.exports = function(sequelize, DataTypes) {
     amount: {type: DataTypes.DECIMAL(10,3), allowNull: false,  unique: false},
     price: {type: DataTypes.DECIMAL(10,2), allowNull: false,  unique: false},
     tax: {type: DataTypes.DECIMAL(10,2), allowNull:false, unique:false},
-    sortId: {type: DataTypes.INTEGER, allowNull:true, unique:true}
+    sortId: {type: DataTypes.INTEGER, allowNull:true, unique:false}
   }, {
     classMethods: {
       associate: function(models) {
