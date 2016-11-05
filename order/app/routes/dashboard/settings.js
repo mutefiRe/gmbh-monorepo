24a49d89aee9e5ddc9e47efd8f2dc89744acdcbc
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2016-11-05 16:20:36 +0100

    Refactor Login / Remove dashboard

--- order/app/routes/dashboard/settings.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import AuthenticatedRouteMixin from '../../mixins/authenticated-route-mixin';
-
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model() {
-    return this.store.findAll('setting');
-  }
-});
