bcc88907ff394a85e1953fbbbaaf90f0d231a153
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2016-11-14 23:25:31 +0100

    feature: update ember to 2.9.1 and extract eslint

--- order/config/environment.js
+++ order/config/environment.js
@@ -2,7 +2,7 @@ module.exports = function (environment) {
   const ENV = {
     modulePrefix: 'gmbh',
     environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -51,7 +51,7 @@ module.exports = function (environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
+    ENV.rootURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
--- order/tests/helpers/start-app.js
+++ order/tests/helpers/start-app.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
-// import './login';
 
 export default function startApp(attrs) {
   let application;
@@ -9,7 +8,7 @@ export default function startApp(attrs) {
   let attributes = Ember.merge({}, config.APP);
   attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
-  Ember.run(()=> {
+  Ember.run(() => {
     application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
