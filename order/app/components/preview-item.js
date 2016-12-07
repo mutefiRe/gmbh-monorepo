e4c7ad68bd03182a5d1c2cde603f8b98ae1c7ac5
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2016-12-07 16:36:51 +0100

    refactor responsive design orderscreen

--- order/app/components/preview-item.js
+++ order/app/components/preview-item.js
@@ -9,5 +9,15 @@ export default Ember.Component.extend({
     const numberOfStyles = 4;
     const style = this.get('item.item.category.id');
     this.set('myStyle', `style-${(style % numberOfStyles)}`);
+  },
+  didUpdate() {
+    const $this = this.$();
+    $this.animate({
+      opacity: 0.5
+    },100, function(){
+      $this.animate({
+        opacity: 1
+      },100);
+    });
   }
 });
