ed49b451f59c08ac90609a6efe0cc0bf74d429be
Daniel <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2016-12-21 13:49:51 +0100

    Refactor: table sorting

--- order/app/components/table-select.js
+++ order/app/components/table-select.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
 
-function tryParse(input) {
-  if (isNaN(input)) return input;
-
-  return parseInt(input);
-}
-
 export default Ember.Component.extend({
   store: Ember.inject.service('store'),
   classNames: ['table-select'],
@@ -19,13 +13,11 @@ export default Ember.Component.extend({
     return area.get('user.id') != this.get('currentUser').id
   }).property('areas'),
   unassignedTables: Ember.computed.filter('tables', function(table){
-    console.log(table.get('area.id'))
     return table.get('custom') == false && !table.get('area.id');
   }).property('tables'),
   customTables: Ember.computed.filter('tables', function(table){
     return table.get('custom') == true;
   }).property('tables.@each.custom'),
-  filter: '',
   actions: {
     setTable(table) {
       this.set('order.table', table);
@@ -58,39 +50,3 @@ export default Ember.Component.extend({
     }
   }
 });
-
-/*
-function sortFunction(a, b){
-  const userAreas = this.get('currentUser').get('areas').toArray().map((x)=> x.id);
-  const rx = /\d+|\D+/g;
-  const aMatches = a.get('name').toString().match(rx);
-  const bMatches = b.get('name').toString().match(rx);
-
-  if (userAreas.indexOf(a.get('area').get('id').toString()) != -1){
-    if (a.get('area').get('name') > b.get('area').get('name')) return 1;
-    else if (a.get('area').get('name') == b.get('area').get('name')){
-      if (aMatches == bMatches) return 0;
-      for (let i = 0; i < aMatches.length && i < bMatches.length; i++)
-      {
-        if (aMatches[i] == bMatches[i]) continue;
-        if (tryParse(aMatches[i]) > tryParse(bMatches[i])) return 1;
-        return -1;
-      }
-    } else return -1;
-  }
-  if (a.get('area').get('name') > b.get('area').get('name')) return 1
-  else if (a.get('area').get('name') == b.get('area').get('name')){
-    if (aMatches == bMatches) return 0
-    for (let i = 0; i < aMatches.length && i < bMatches.length; i++)
-    {
-      if (aMatches[i] == bMatches[i])
-      {
-        continue;
-      }
-      if (tryParse(aMatches[i]) > tryParse(bMatches[i])) return 1
-      return -1;
-    }
-  } else return -1;
-  return -1;
-}
-*/
