00a79c5afef14f4a3dcfb182a5412bc1cbe0a164
Daniel <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2017-01-10 21:57:11 +0100

    feat: user items

---
+++ dashboard/tests/unit/helpers/eq-test.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+
+import { describe, it } from 'mocha';
+import { eq } from 'admin/helpers/eq';
+
+describe('Unit | Helper | eq', function() {
+  // Replace this with your real tests.
+  it('works', function() {
+    let result = eq(42);
+    expect(result).to.be.ok;
+  });
+});
+
