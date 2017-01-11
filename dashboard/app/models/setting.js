c1e19717085cb1cd3f4426f4c8e973ca44437737
Sebastian Huber <Seb@c78-104-169-41.fh-salzburg.ac.at>
2017-01-11 22:55:00 +0100

    add models

---
+++ dashboard/app/models/setting.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  beginDate: DS.attr('date'),
+  endDate: DS.attr('date'),
+  instantPay: DS.attr('boolean')
+});
---
+++ dashboard/app/models/table.js
@@ -0,0 +1,19 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  x: DS.attr('number'),
+  y: DS.attr('number'),
+  createdAt: DS.attr('date'),
+  updatedAt: DS.attr('date'),
+  area: DS.belongsTo('area'),
+  numericID: function () {
+    const id = this.get('id');
+
+    if (id) {
+      return Number(id);
+    }
+
+    return null;
+  }.property('id')
+});
