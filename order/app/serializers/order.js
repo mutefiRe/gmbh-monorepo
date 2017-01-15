d39d75b8f7a7f59c5cdbe13552305e00f2746342
Daniel <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2017-01-15 19:06:02 +0100

    chore: remove console log

--- order/app/serializers/order.js
+++ order/app/serializers/order.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
   keyForRelationship(key, relationship) {
-    console.log(key, relationship);
     if (relationship === 'belongsTo') {
       return `${key}Id`;
     }
