404118984d4e090defc292dfb3652c484d103786
Daniel <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2017-01-15 13:48:18 +0100

    feat: add relationship serializing

---
+++ order/app/serializers/application.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.RESTSerializer.extend({
+  keyForRelationship(key, relationship) {
+    if (relationship === 'belongsTo') {
+      return `${key}Id`;
+    }
+  }
+});
