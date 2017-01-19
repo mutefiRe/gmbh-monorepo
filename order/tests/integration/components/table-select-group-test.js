44b0c9aa2642a56d981617ece502317f8dbf557e
Konrad Kleeberger <git@konrad.online>
2017-01-19 22:48:13 +0100

    hotfix: deprecation warning ember-mocha

--- order/tests/integration/components/table-select-group-test.js
+++ order/tests/integration/components/table-select-group-test.js
@@ -1,24 +1,24 @@
 import { expect } from 'chai';
-import { describeComponent, it } from 'ember-mocha';
+import { it, describe } from 'mocha';
+import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
-describeComponent('table-select-group', 'Integration | Component | table select group',
-  {
+describe('Integration | Component | table select group', function() {
+  setupComponentTest('table-select-group', {
     integration: true
-  },
-  function() {
-    it('renders', function() {
-      // Set any properties with this.set('myProperty', 'value');
-      // Handle any actions with this.on('myAction', function(val) { ... });
-      // Template block usage:
-      // this.render(hbs`
-      //   {{#table-select-group}}
-      //     template content
-      //   {{/table-select-group}}
-      // `);
+  });
 
-      this.render(hbs`{{table-select-group}}`);
-      expect(this.$()).to.have.length(1);
-    });
-  }
-);
+  it('renders', function() {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+    // Template block usage:
+    // this.render(hbs`
+    //   {{#table-select-group}}
+    //     template content
+    //   {{/table-select-group}}
+    // `);
+
+    this.render(hbs`{{table-select-group}}`);
+    expect(this.$()).to.have.length(1);
+  });
+});
