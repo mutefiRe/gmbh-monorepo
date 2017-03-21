68528a6a2b7112da8740abd59a0d4546f9ae2968
Alexander <office@gabriel-alexander.at>
2017-03-21 17:03:51 +0100

    feat: table payment

--- order/app/components/pay-detail/main-view.js
+++ order/app/components/pay-detail/main-view.js
@@ -2,23 +2,26 @@ import RecognizerMixin from 'ember-gestures/mixins/recognizers';
 import Ember from 'ember';
 
 export default Ember.Component.extend(RecognizerMixin, {
-  modal:           Ember.inject.service(),
+  modal: Ember.inject.service(),
   pageTransitions: Ember.inject.service('pagetransitions'),
-  classNames:      ['order-detail-view','screen'],
-  recognizers:     'swipe',
-  paidOrderitems: Ember.computed.filter('order.orderitems', function(orderitem) {
+  classNames: ['order-detail-view', 'screen'],
+  recognizers: 'swipe',
+  type: Ember.computed('order', function () {
+    return this.get('order.constructor.modelName');
+  }),
+  paidOrderitems: Ember.computed.filter('order.orderitems.@each.countPaid', function (orderitem) {
     if (orderitem.get('countPaid') > 0) return true;
     return false;
   }),
-  markedOrderitems: Ember.computed.filter('order.orderitems.@each.countMarked', function(orderitem) {
+  markedOrderitems: Ember.computed.filter('order.orderitems.@each.countMarked', function (orderitem) {
     if (orderitem.get('countMarked') > 0) return true;
     return false;
   }),
-  orderitems: Ember.computed.filter('order.orderitems.@each.countMarked', function(orderitem) {
+  orderitems: Ember.computed.filter('order.orderitems.@each.{countMarked,countPaid}', function (orderitem) {
     if (orderitem.get('countPaid') + orderitem.get('countMarked') < orderitem.get('count')) return true;
     return false;
   }),
-  markedAmount: Ember.computed('markedOrderitems', function(){
+  markedAmount: Ember.computed('markedOrderitems', function () {
     const orderitems = this.get('markedOrderitems');
     let sum = 0;
     for (const orderitem of orderitems) {
@@ -26,15 +29,10 @@ export default Ember.Component.extend(RecognizerMixin, {
     }
     return sum;
   }),
-  openAmount: Ember.computed('orderitems', function(){
-    const orderitems = this.get('orderitems');
-    let sum = 0;
-    for (const orderitem of orderitems) {
-      sum += orderitem.get('price') * (orderitem.get('count') - orderitem.get('countPaid'));
-    }
-    return sum;
+  openAmount: Ember.computed('order', 'order.openAmount', function () {
+    return this.get('order.openAmount');
   }),
-  open: Ember.computed('openAmount', function(){
+  open: Ember.computed('openAmount', function () {
     return this.get('openAmount') > 0;
   }),
   forFree: false,
@@ -42,10 +40,10 @@ export default Ember.Component.extend(RecognizerMixin, {
     this.goToOrderOverview();
   },
   goToOrderScreen() {
-    this.get('pageTransitions').toScreen({screen: 'order-screen', from: 'right'});
+    this.get('pageTransitions').toScreen({ screen: 'order-screen', from: 'right' });
   },
   goToOrderOverview() {
-    this.get('pageTransitions').toScreen({screen: 'order-overview', from: 'left'});
+    this.get('pageTransitions').toScreen({ screen: 'order-overview', from: 'left' });
   },
   actions: {
     goToOrderScreen() {
@@ -54,32 +52,35 @@ export default Ember.Component.extend(RecognizerMixin, {
     goToOrderOverview() {
       this.goToOrderOverview();
     },
-    paySelected(){
+    paySelected() {
       this.get('modal').showModal({ activeType: 'loading-box' });
       const orderitems = this.get('markedOrderitems');
       const forFreeOrder = this.get('forFree');
-      let promises = [];
-      for(let orderitem of orderitems){
+      for (const orderitem of orderitems) {
         orderitem.set('countPaid', orderitem.get('countPaid') + orderitem.get('countMarked'));
         if (forFreeOrder) orderitem.set('countFree', orderitem.get('countFree') + orderitem.get('countMarked'));
         orderitem.set('countMarked', 0);
       }
 
-      let order = this.get('order');
-      let items = order.get('orderitems');
+      let orders;
 
-      order.set('isPaid', this.get('openAmount') == 0);
-      order.set('totalAmount', this.get('openAmount'));
-      order.save().then(() => {
+      if (this.get('type') === 'table') {
+        orders = this.get('order.orders');
+      } else {
+        orders = [this.get('order')];
+      }
+
+      const promises = orders.map(order => {
+        order.set('totalAmount', order.get('openAmount'));
+        return order.save();
+      });
+
+      Promise.all(promises).then(() => {
         this.get('modal').closeModal();
         this.set('forFree', false);
-      }).catch((err) => {
-        this.get('order.orderitems').forEach((item) => {
-          item.rollbackAttributes();
-        });
-        this.get('order').rollbackAttributes();
       });
     },
+
     payAll() {
       this.get('modal').showModal({ activeType: 'loading-box' });
       const orderitems = this.get('order.orderitems');
@@ -87,23 +88,30 @@ export default Ember.Component.extend(RecognizerMixin, {
 
       orderitems.forEach(item => {
         item.set('countPaid', item.get('count'));
+        item.set('countMarked',  item.get('count'));
         item.set('countMarked', 0);
         if (forFree) item.set('countFree', item.get('count'));
       });
 
-      const order = this.get('order');
+      let orders;
 
-      order.set('isPaid', true);
-      order.set('totalAmount', 0);
-      order.save().then(() => {
+      if (this.get('type') === 'table') {
+        orders = this.get('order.orders');
+      } else {
+        orders = [this.get('order')];
+      }
+
+      const promises = orders.map(order => {
+        order.set('isPaid', true);
+        order.set('totalAmount', 0);
+        return order.save();
+      });
+
+      Promise.all(promises).then(() => {
         this.get('modal').closeModal();
         this.set('forFree', false);
-      }).catch(() => {
-        this.get('order.orderitems').forEach(item => {
-          item.rollbackAttributes();
-        });
-        this.get('order').rollbackAttributes();
       });
+
     },
     printBill() {
       this.get('printBill')(this.get('order').id);
--- order/app/models/order.js
+++ order/app/models/order.js
@@ -5,6 +5,14 @@ export default DS.Model.extend({
   updatedAt: DS.attr('date'),
   user: DS.belongsTo('user'),
   table: DS.belongsTo('table'),
-  orderitems: DS.hasMany('orderitem')
+  orderitems: DS.hasMany('orderitem'),
+  openAmount: Ember.computed('orderitems', function () {
+    const orderitems = this.get('orderitems').toArray();
+    let sum = 0;
+    for (const orderitem of orderitems) {
+      sum += orderitem.get('price') * (orderitem.get('count') - orderitem.get('countPaid'));
+    }
+    return sum;
+  })
 });
 
