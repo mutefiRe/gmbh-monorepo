3e40d8ea1169b2976ad37c01b87d6bd7a0fe68c5
Konrad Kleeberger <git@konrad.online>
2017-03-23 16:13:28 +0100

    feat: log ember exceptions

---
+++ api/router/error.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+router.post('/', function(req, res){
+  console.error(`Ember Error in ${req.body.app} with message: "${req.body.message}"`); //eslint-disable-line
+  res.status(200).send({});
+});
+
+module.exports = router;
