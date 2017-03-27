48d318db7a12608f5167d4a18b750c43da031339
Daniel <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2017-03-27 13:57:14 +0200

    fix: and gerefactoren instantpay for cashier and gerenamen

--- order/app/models/user.js
+++ order/app/models/user.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   username:   DS.attr('string'),
@@ -7,5 +8,8 @@ export default DS.Model.extend({
   password:   DS.attr('string'),
   permission: DS.attr('permissions'),
   areas:      DS.hasMany('area'),
-  printer:    DS.attr('string')
+  printer:    DS.attr('string'),
+  isCashier:  Ember.computed('printer', function(){
+    return this.get('printer') ? true : false;
+  })
 });
