f98d68a4f282e42e0992b2280e8906f965a121f6
Unknown <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-03-27 10:41:30 +0200

    feat: adds coffee category and products

--- api/seed/seed.js
+++ api/seed/seed.js
@@ -1,47 +1,75 @@
-'use strict';
+"use strict";
 
-const db = require('../models/index');
-const faker = require('faker2');
+const db = require("../models/index");
+const faker = require("faker2");
 
-module.exports = function(){
-  const speisen = [ 'Schnitzel',
-    'Käsekrainer',
-    'Schweinebraten',
-    'Pommes',
-    'Toast',
-    'Waffeln',
-    'Wurst',
-    'Currywurst',
-    'Hendl',
-    'Frankfurter',
-    'Gulasch',
-    'Leberkassemmel',
-    'Kartoffelpuffer',
-    'Steak',
-    'Bananenquiche',
-    'Knödel' ];
+module.exports = function() {
+  const speisen = [
+    "Schnitzel",
+    "Käsekrainer",
+    "Schweinebraten",
+    "Pommes",
+    "Toast",
+    "Waffeln",
+    "Wurst",
+    "Currywurst",
+    "Hendl",
+    "Frankfurter",
+    "Gulasch",
+    "Leberkassemmel",
+    "Kartoffelpuffer",
+    "Steak",
+    "Bananenquiche",
+    "Knödel"
+  ];
 
-  const stopwords = ['in','an','auf','mit', 'überbacken mit'];
+  const stopwords = ["in", "an", "auf", "mit", "überbacken mit"];
 
-  const beilagen = [ 'Salat',
-    'Püree',
-    'Kartoffel',
-    'Reis',
-    'Nudeln',
-    'Bratgemüse',
-    'Pumpernickel',
-    'Frühlingsgemüse',
-    'Brot',
-    'Weißbrot',
-    'Breze',
-    'Rettich',
-    'Sauerkraut',
-    'Semmel',
-    'Soße',
-    'Himbeerspiegel',
-    'Preiselbeeren',
-    'Erdäpfelsalat',
-    'Pommes' ];
+  const beilagen = [
+    "Salat",
+    "Püree",
+    "Kartoffel",
+    "Reis",
+    "Nudeln",
+    "Bratgemüse",
+    "Pumpernickel",
+    "Frühlingsgemüse",
+    "Brot",
+    "Weißbrot",
+    "Breze",
+    "Rettich",
+    "Sauerkraut",
+    "Semmel",
+    "Soße",
+    "Himbeerspiegel",
+    "Preiselbeeren",
+    "Erdäpfelsalat",
+    "Pommes"
+  ];
+
+  const kaffee = [
+    "Espresso",
+    "Doppelter-Espresso",
+    "Caffe Latte",
+    "Capuccino",
+    "Mokkaccino",
+    "Mokka Latte",
+    "Verlängerter",
+    "Chai Latte"
+  ];
+
+  const desert = [
+    "Schwarzwälder-Kirsch Torte",
+    "Topfenstrudel",
+    "Apfelstrudel",
+    "Guglhupf",
+    "Schokomouse",
+    "Deserteis",
+    "Birnenparfait",
+    "Bananensplit",
+    "Krokantbecher",
+    "Heiße Liebe"
+  ];
 
   /* USER */
 
@@ -68,7 +96,7 @@ module.exports = function(){
     permission: 1
   });
 
-  for(let i = 0; i < 50; i++){
+  for (let i = 0; i < 50; i++) {
     db.User.create({
       username: faker.Internet.userName(),
       firstname: faker.Name.firstName(),
@@ -80,30 +108,37 @@ module.exports = function(){
 
   /* FOOD */
 
-  db.Category.create({
-    name: "Speisen",
-    enabled: true,
-    icon: "food",
-    description: "Alle die Guten Sachen",
-    showAmount: false,
-    printer: "GMBH-WLAN"
-  }).then(cat => {
-
-    db.Unit.create({
-      name: "Stk."
-    }).then(data => {
-      for(let i = 0; i < 25; i++){
-        db.Item.create({
-          name: faker.Helpers.randomize(speisen) + " " + faker.Helpers.randomize(stopwords) + " " + faker.Helpers.randomize(beilagen),
-          amount: 1,
-          price: faker.Helpers.randomNumber(20),
-          tax: 0.1,
-          unitId: data.id,
-          categoryId: cat.id
+  db.Category
+    .create({
+      name: "Speisen",
+      enabled: true,
+      icon: "food",
+      description: "Alle die Guten Sachen",
+      showAmount: false,
+      printer: "GMBH-WLAN"
+    })
+    .then(cat => {
+      db.Unit
+        .create({
+          name: "Stk."
+        })
+        .then(data => {
+          for (let i = 0; i < 25; i++) {
+            db.Item.create({
+              name: faker.Helpers.randomize(speisen) +
+                " " +
+                faker.Helpers.randomize(stopwords) +
+                " " +
+                faker.Helpers.randomize(beilagen),
+              amount: 1,
+              price: faker.Helpers.randomNumber(20),
+              tax: 0.1,
+              unitId: data.id,
+              categoryId: cat.id
+            });
+          }
         });
-      }
     });
-  });
 
   /* ORGANISATIONS */
 
@@ -132,7 +167,7 @@ module.exports = function(){
     begin_date: "nodate",
     end_date: "nodate",
     instantPay: true,
-    customTables: false,
+    customTables: true,
     receiptPrinter: "GMBH-WLAN",
     eventName: "GMBH",
     expiresTime: "72h"
@@ -140,232 +175,298 @@ module.exports = function(){
 
   /* ALCOHOLICS */
 
-  db.Category.create({
-    name: "Alkoholisches",
-    enabled: true,
-    icon: "drink-alc",
-    description: "alkohol",
-    showAmount: true,
-    printer: "GMBH-WLAN"
-  }).then(cat => {
-    db.Unit.findOrCreate({
-      where: {name: 'l'}
-    }).spread(data => {
-      db.Item.bulkCreate([{
-        name: "Bier",
-        amount: 0.5,
-        price: 3.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Bier",
-        amount: 0.33,
-        price: 2.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Radler",
-        amount: 0.5,
-        price: 2.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Radler",
-        amount: 0.3,
-        price: 2.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Grüner Veltliner",
-        amount: 0.125,
-        price: 3.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Grüner Veltliner",
-        amount: 0.25,
-        price: 5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Spätburgunder",
-        amount: 0.125,
-        price: 3.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Spätburgunder",
-        amount: 0.25,
-        price: 5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Weißer Spritzer",
-        amount: 0.5,
-        price: 3.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Roter Spritzer",
-        amount: 0.5,
-        price: 3.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      }]
-      );
-    });
+  db.Category
+    .create({
+      name: "Alkoholisches",
+      enabled: true,
+      icon: "drink-alc",
+      description: "alkohol",
+      showAmount: true,
+      printer: "GMBH-WLAN"
+    })
+    .then(cat => {
+      db.Unit
+        .findOrCreate({
+          where: { name: "l" }
+        })
+        .spread(data => {
+          db.Item.bulkCreate([
+            {
+              name: "Bier",
+              amount: 0.5,
+              price: 3.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Bier",
+              amount: 0.33,
+              price: 2.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Radler",
+              amount: 0.5,
+              price: 2.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Radler",
+              amount: 0.3,
+              price: 2.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Grüner Veltliner",
+              amount: 0.125,
+              price: 3.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Grüner Veltliner",
+              amount: 0.25,
+              price: 5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Spätburgunder",
+              amount: 0.125,
+              price: 3.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Spätburgunder",
+              amount: 0.25,
+              price: 5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Weißer Spritzer",
+              amount: 0.5,
+              price: 3.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Roter Spritzer",
+              amount: 0.5,
+              price: 3.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            }
+          ]);
+        });
 
-    db.Unit.findOrCreate({
-      where: {name: 'cl'}
-    }).spread(data => {
-      db.Item.bulkCreate([{
-        name: "Klopfer",
-        amount: 2,
-        price: 2.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Jägermeister",
-        amount: 2,
-        price: 2.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Obstler",
-        amount: 4,
-        price: 2,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Williams-Schnaps",
-        amount: 4,
-        price: 2,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Nuss-Schnaps",
-        amount: 4,
-        price: 2,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      }]
-      );
+      db.Unit
+        .findOrCreate({
+          where: { name: "cl" }
+        })
+        .spread(data => {
+          db.Item.bulkCreate([
+            {
+              name: "Klopfer",
+              amount: 2,
+              price: 2.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Jägermeister",
+              amount: 2,
+              price: 2.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Obstler",
+              amount: 4,
+              price: 2,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Williams-Schnaps",
+              amount: 4,
+              price: 2,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Nuss-Schnaps",
+              amount: 4,
+              price: 2,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            }
+          ]);
+        });
     });
-  });
 
   /* NONALCOHOLICS */
 
-  db.Category.create({
-    name: "Alkoholfreies",
-    enabled: true,
-    icon: "drink-anti",
-    description: "Alkoholfreies",
-    showAmount: true,
-    printer: "GMBH-WLAN"
-  }).then(cat => {
-    db.Unit.findOrCreate({
-      where: {name: 'l'}
-    }).spread(data => {
-      db.Item.bulkCreate([{
-        name: "Coca Cola",
-        amount: 0.3,
-        price: 2.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Coca Cola",
-        amount: 0.5,
-        price: 3.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Fanta",
-        amount: 0.3,
-        price: 2.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Fanta",
-        amount: 0.5,
-        price: 3.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Sprite",
-        amount: 0.3,
-        price: 2.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Sprite",
-        amount: 0.5,
-        price: 3.5,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Mineral",
-        amount: 0.3,
-        price: 2,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      },{
-        name: "Mineral",
-        amount: 0.5,
-        price: 3,
-        tax: 0.2,
-        unitId: data.id,
-        categoryId: cat.id
-      }]
-      );
+  db.Category
+    .create({
+      name: "Kaffee",
+      enabled: true,
+      icon: "drink-coffee",
+      description: "Kaffee",
+      showAmount: true,
+      printer: "GMBH-WLAN"
+    })
+    .then(cat => {
+      db.Unit
+        .findOrCreate({
+          where: { name: "l" }
+        })
+        .spread(data => {
+          db.Item.bulkCreate(
+            kaffee.map(x => {
+              return {
+                name: x,
+                amount: 1,
+                price: (Math.round(Math.random() * 20 * 10) / 10).toFixed(2),
+                tax: 0.2,
+                unitId: data.id,
+                categoryId: cat.id
+              };
+            })
+          );
+        });
     });
-  });
 
+  db.Category
+    .create({
+      name: "Alkoholfreies",
+      enabled: true,
+      icon: "drink-anti",
+      description: "Alkoholfreies",
+      showAmount: false,
+      printer: "GMBH-WLAN"
+    })
+    .then(cat => {
+      db.Unit
+        .findOrCreate({
+          where: { name: "Stk." }
+        })
+        .spread(data => {
+          db.Item.bulkCreate([
+            {
+              name: "Coca Cola",
+              amount: 0.3,
+              price: 2.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Coca Cola",
+              amount: 0.5,
+              price: 3.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Fanta",
+              amount: 0.3,
+              price: 2.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Fanta",
+              amount: 0.5,
+              price: 3.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Sprite",
+              amount: 0.3,
+              price: 2.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Sprite",
+              amount: 0.5,
+              price: 3.5,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Mineral",
+              amount: 0.3,
+              price: 2,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            },
+            {
+              name: "Mineral",
+              amount: 0.5,
+              price: 3,
+              tax: 0.2,
+              unitId: data.id,
+              categoryId: cat.id
+            }
+          ]);
+        });
+    });
 
-  db.Area.findOrCreate({
-    where: {name: 'Terrasse'}
-  }).spread(area => {
-    for(let i = 0; i < 12; i++){
-      db.Table.create({
-        name:"T" + i,
-        x: 1,
-        y: 2,
-        areaId: area.id
-      });
-    }
-  });
+  db.Area
+    .findOrCreate({
+      where: { name: "Terrasse" }
+    })
+    .spread(area => {
+      for (let i = 0; i < 12; i++) {
+        db.Table.create({
+          name: "T" + i,
+          x: 1,
+          y: 2,
+          areaId: area.id
+        });
+      }
+    });
 
-  db.Area.findOrCreate({
-    where: {name: 'Gaststube'}
-  }).spread(area => {
-    for(let i = 0; i < 12; i++){
-      db.Table.create({
-        name:"G" + i,
-        x: 1,
-        y: 2,
-        areaId: area.id
-      });
-    }
-  });
+  db.Area
+    .findOrCreate({
+      where: { name: "Gaststube" }
+    })
+    .spread(area => {
+      for (let i = 0; i < 12; i++) {
+        db.Table.create({
+          name: "G" + i,
+          x: 1,
+          y: 2,
+          areaId: area.id
+        });
+      }
+    });
 };
