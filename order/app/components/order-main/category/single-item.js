aedb193834c1219f01b3a2c7519f3b0523d8eef7
Unknown <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-03-29 13:53:43 +0200

    feat: adds new colors to items

--- order/app/components/order-main/category/single-item.js
+++ order/app/components/order-main/category/single-item.js
@@ -3,17 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'div',
   classNames: ['category_single-item'],
-  classNameBindings: ['myStyle'],
-  myStyle: function() {
-    if (this.get('actualCategory') === this.get('category') || !this.get('actualCategory')) {
-      return 'category-active';
+  attributeBindings: ['style'],
+  style: Ember.computed('category', 'actualCategory', function(){
+    if(this.get('actualCategory') === this.get('category') || !this.get('actualCategory')) {
+      return 'color: ' + this.get('category.textcolor') + '; background-color: ' + this.get('category.color');
     }
-
-    return 'category-inactive';
-  }.property('actualCategory'),
-  actions: {
-
-  },
+    return 'color: ' + this.get('category.textcolor') + '; filter: grayscale(70%); background-color: ' + this.get('category.color');
+  }),
   click() {
     this.get('changeCategory')(this.get('category'));
   }
