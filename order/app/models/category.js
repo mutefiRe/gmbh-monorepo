1a12222db1a0bc97c5be7fc9ab3ae0e50492bfe0
Unknown <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-03-29 13:53:12 +0200

    feat: adds color property to ember model and computed color for inner text

--- order/app/models/category.js
+++ order/app/models/category.js
@@ -1,16 +1,26 @@
-import DS from 'ember-data';
+import DS from "ember-data";
 
 export default DS.Model.extend({
-  name:         DS.attr('string'),
-  enabled:      DS.attr('boolean'),
-  description:  DS.attr('string'),
-  createdAt:    DS.attr('date'),
-  updatedAt:    DS.attr('date'),
-  showAmount:   DS.attr('boolean'),
-  icon:         DS.attr('string'),
-  printer:      DS.attr('string'),
-  category:     DS.belongsTo('category'),
-  items:        DS.hasMany('item'),
-  itemsSorting: ['sortId', 'name'],
-  sortedItems:  Ember.computed.sort('items', 'itemsSorting')
+  name: DS.attr("string"),
+  enabled: DS.attr("boolean"),
+  description: DS.attr("string"),
+  createdAt: DS.attr("date"),
+  updatedAt: DS.attr("date"),
+  showAmount: DS.attr("boolean"),
+  icon: DS.attr("string"),
+  printer: DS.attr("string"),
+  category: DS.belongsTo("category"),
+  items: DS.hasMany("item"),
+  itemsSorting: ["sortId", "name"],
+  sortedItems: Ember.computed.sort("items", "itemsSorting"),
+  color: DS.attr("string"),
+  textcolor: Ember.computed("color", function() {
+    const color = this.get("color");
+    const R = parseInt(color.substring(1, 3), 16);
+    const G = parseInt(color.substring(3, 5), 16);
+    const B = parseInt(color.substring(5, 7), 16);
+    const luminance = 0.2126 * R + 0.7152 * G + 0.0722 * B;
+    return luminance > 127 ? "black" : "white";
+  })
 });
+
