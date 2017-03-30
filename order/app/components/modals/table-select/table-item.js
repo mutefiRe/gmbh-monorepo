5d50200daa2f6a21e39f6631e8279406148a7c15
Unknown <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-03-30 17:58:04 +0200

    feat: add custom color of area to tables

--- order/app/components/modals/table-select/table-item.js
+++ order/app/components/modals/table-select/table-item.js
@@ -3,15 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'button',
   classNames: ['table-item'],
-  classNameBindings: ['styleType'],
-  styleType: 'style-1',
-  init() {
-    this._super();
-    const numberOfStyles = 4;
-
-    const areaId = this.get('table').get('area').get('id');
-    this.set('styleType', `style-${(areaId ? areaId : 1 % numberOfStyles)}`);
-  },
+  attributeBindings: ['style'],
+  style: Ember.computed('table.area', function(){
+    return 'color: ' + this.get('table.area.textcolor') + '; background-color: ' + this.get('table.area.color');
+  }),
   click() {
     this.get('setTable')(this.get('table'));
   }
--- order/app/components/pay-main/single-table.js
+++ order/app/components/pay-main/single-table.js
@@ -1,8 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['pay-main_single-table','style-1'],
+  classNames: ['pay-main_single-table'],
   tagName: 'div',
+  attributeBindings: ['style'],
+  style: Ember.computed('table.area', function(){
+    return 'color: ' + this.get('table.area.textcolor') + '; background-color: ' + this.get('table.area.color');
+  }),
   click() {
     this.set('actualOrder', this.get('table'));
     this.get('goToOrderDetail')();
