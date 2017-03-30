c22f1e00bced20d91a320f3dca5d43ba4b590b74
Unknown <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-03-30 17:58:42 +0200

    feat: add custom color property to area store

--- order/app/models/area.js
+++ order/app/models/area.js
@@ -5,5 +5,14 @@ export default DS.Model.extend({
   updatedAt: DS.attr('date'),
   createdAt: DS.attr('date'),
   tables:    DS.hasMany('table'),
-  users:     DS.hasMany('user')
+  users:     DS.hasMany('user'),
+  color: DS.attr("string", {defaultValue: '#aabbcc'}),
+  textcolor: Ember.computed("color", function() {
+    const color = this.get("color");
+    const R = parseInt(color.substring(1, 3), 16);
+    const G = parseInt(color.substring(3, 5), 16);
+    const B = parseInt(color.substring(5, 7), 16);
+    const luminance = 0.2126 * R + 0.7152 * G + 0.0722 * B;
+    return luminance > 127 ? "black" : "white";
+  })
 });
