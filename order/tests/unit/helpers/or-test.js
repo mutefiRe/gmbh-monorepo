1d619cf22d9e7753c996304d376ff17b4a595afe
Unknown <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-04-05 15:59:54 +0200

    feat: logical or helper for handlebars

---
+++ order/tests/unit/helpers/or-test.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { or } from 'gmbh/helpers/or';
+
+describe('Unit | Helper | or', function() {
+  // Replace this with your real tests.
+  it('works', function() {
+    let result = or(42);
+    expect(result).to.be.ok;
+  });
+});
+
