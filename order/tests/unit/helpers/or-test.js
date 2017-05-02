e2dfda649474585cc14fbbd0fd41cb7d7366f1af
Unknown <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-05-02 10:37:52 +0200

    chore: fix const declaration in test linting

--- order/tests/unit/helpers/or-test.js
+++ order/tests/unit/helpers/or-test.js
@@ -5,7 +5,7 @@ import { or } from 'gmbh/helpers/or';
 describe('Unit | Helper | or', function() {
   // Replace this with your real tests.
   it('works', function() {
-    let result = or(42);
+    const result = or(42);
     expect(result).to.be.ok;
   });
 });
