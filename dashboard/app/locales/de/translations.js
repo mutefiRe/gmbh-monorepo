93387b6fbdab38814eb1c9fa9a7937c83df3ae00
Josef Krabath <jkrabath.mmt-b2014@fh-salzburg.ac.at>
2017-05-03 17:26:21 +0200

    feat: implemented main-navigation
    + main-navigation
    + main-header
    + i18n
    + routes for category, event, printer, product, table, user

---
+++ dashboard/app/locales/de/translations.js
@@ -0,0 +1,11 @@
+export default {
+  'headlines': {
+    'categories': 'Kategorien',
+    'events':     'Veranstaltungen',
+    'logout':     'Logout',
+    'printers':   'Drucker',
+    'products':   'Produkte',
+    'tables':     'Tische',
+    'users':      'User'
+  }
+};
