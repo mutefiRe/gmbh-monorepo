05505a9626953199c25a263e893bb5138a14706b
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-07 13:27:38 +0200

    add: existing categories to product dropdown

--- dashboard/app/routes/products.js
+++ dashboard/app/routes/products.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('item');
+    return Ember.RSVP.hash({
+      categories: this.store.findAll('category'),
+      products: this.store.findAll('item'),
+    });
   }
 });
