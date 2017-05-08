15a55cbd5d44186809837c526e29f8646309794d
Josef Krabath <jkrabath.mmt-b2014@fh-salzburg.ac.at>
2017-05-08 15:11:35 +0200

    feat: update user obj when pw is null

--- api/router/api/users.js
+++ api/router/api/users.js
@@ -126,6 +126,7 @@ router.put('/:id', function(req, res) {
   const io = req.app.get('io');
   db.User.find({where: {id: req.params.id}}).then(user => {
     if (user === null) throw new Error('user not found');
+    if (req.body.user.password === null) Reflect.deleteProperty(req.body.user, 'password');
     return user.update(req.body.user);
   }).then(user => {
     res.send({user});
