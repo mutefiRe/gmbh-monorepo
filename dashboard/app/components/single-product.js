c81abd06f0d1a0363fcbf1bd1dec5ae14c7faafb
Unknown <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-05-08 19:38:43 +0200

    fix: remove useless console log

--- dashboard/app/components/single-product.js
+++ dashboard/app/components/single-product.js
@@ -3,11 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   editable: Ember.inject.service(),
   tagName: 'li',
-  init() {
-    this._super();
-    console.log('herbert');
-
-  },
   actions: {
     toggleEditable() {
       this.get('editable').toggle(this);
