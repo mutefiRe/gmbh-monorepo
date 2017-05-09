d90bc4a620a9fe9e7eb909657967b6dd6116ccb2
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-09 14:18:26 +0200

    add: colorpicker modal

--- dashboard/app/components/single-category.js
+++ dashboard/app/components/single-category.js
@@ -2,10 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   editable: Ember.inject.service(),
+  modal: Ember.inject.service(),
   tagName: 'li',
   actions: {
     toggleEditable() {
       this.get('editable').toggle(this);
+    },
+    showModal() {
+      this.get('modal').showModal(this);
+      return false;
     }
   }
 });
