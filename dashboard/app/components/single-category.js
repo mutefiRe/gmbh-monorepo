52291401997347be18d848dd81572f81547cb341
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-10 15:13:46 +0200

    add: iconpicker modal

--- dashboard/app/components/single-category.js
+++ dashboard/app/components/single-category.js
@@ -1,15 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  editable: Ember.inject.service(),
-  modal: Ember.inject.service(),
-  tagName: 'li',
-  actions: {
-    toggleEditable() {
-      this.get('editable').toggle(this);
-    },
-    showModal() {
-      this.get('modal').showModal(this);
+    editable: Ember.inject.service(),
+    modal: Ember.inject.service(),
+    tagName: 'li',
+    actions: {
+        toggleEditable() {
+            this.get('editable').toggle(this);
+        },
+        showModalColor() {
+            this.get('modal').showModal(this, '.color');
+        },
+        showModalIcon() {
+            this.get('modal').showModal(this, '.icon');
+        }
     }
-  }
 });
