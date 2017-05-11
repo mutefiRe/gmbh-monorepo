b84bc3fecbe81e24e664aa82ff443656ea30752a
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-11 18:12:44 +0200

    feat: change color with modal

--- dashboard/app/components/modal-colorpicker.js
+++ dashboard/app/components/modal-colorpicker.js
@@ -1,12 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  modal: Ember.inject.service(),
-  tagName: 'div',
-  classNames: ['modal'],
-  actions: {
-    hideModal() {
-      this.get('modal').hideModal(this);
+    modal: Ember.inject.service(),
+    tagName: 'div',
+    classNames: ['modal'],
+    actions: {
+        hideModal() {
+            this.get('modal').hideModal(this);
+        },
+        changeColor(hex, name) {
+            this.sendAction('action', hex, name);
+        }
     }
-  }
 });
