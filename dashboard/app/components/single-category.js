080dba30a92d1a8398b348e4760eb9b6a9ff1482
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-11 18:34:53 +0200

    feat: make category icon changeable

--- dashboard/app/components/single-category.js
+++ dashboard/app/components/single-category.js
@@ -5,6 +5,8 @@ export default Ember.Component.extend(Ember.Evented, {
     modal: Ember.inject.service(),
     colorname: 'donaublue',
     colorhex: '#3E8BF4',
+    icontype: 'face',
+    iconname: 'Kopf',
     tagName: 'li',
     actions: {
         toggleEditable() {
@@ -17,7 +19,10 @@ export default Ember.Component.extend(Ember.Evented, {
             this.get('modal').showModal(this, '.icon');
         },
         changeColor(hex, name) {
-            this.setProperties({ colorname: name, colorhex: hex });
+            this.setProperties({ colorhex: hex, colorname: name });
+        },
+        changeIcon(type, name) {
+            this.setProperties({ icontype: type, iconname: name })
         }
     }
 });
