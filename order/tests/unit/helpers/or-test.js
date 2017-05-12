cd0cc6ee0b2ecf50f89a4407d4096f1d078ae3fe
Unknown <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-05-12 13:47:37 +0200

    fix: broken or helper test

--- order/tests/unit/helpers/or-test.js
+++ order/tests/unit/helpers/or-test.js
@@ -5,8 +5,7 @@ import { or } from 'gmbh/helpers/or';
 describe('Unit | Helper | or', function() {
   // Replace this with your real tests.
   it('works', function() {
-    const result = or(42);
+    const result = or([42]);
     expect(result).to.be.ok;
   });
 });
-
