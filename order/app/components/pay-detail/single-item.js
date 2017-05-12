d7c01e989aa8ff7cfd2f5786c932c2df3327bdaf
Unknown <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-05-12 10:29:41 +0200

    feat: category indicator to pay-detail/single-item

--- order/app/components/pay-detail/single-item.js
+++ order/app/components/pay-detail/single-item.js
@@ -4,9 +4,13 @@ export default Ember.Component.extend({
   classNames: ['pay-detail_single-item'],
   classNameBindings: ['css'],
   tagName: 'div',
-  computedCount: Ember.computed('orderitem.countMarked', 'orderitem.countPaid', function(){
+  attributeBindings: ['style'],
+  style: Ember.computed('category', function() {
+    return 'border-left: ' + this.get('orderitem.item.category.color') + ' 5px solid';
+  }),
+  computedCount: Ember.computed('orderitem.countMarked', 'orderitem.countPaid', function() {
     const orderitem = this.get('orderitem');
-    switch(this.get('type')){
+    switch (this.get('type')) {
       case "paid":
         return orderitem.get('countPaid');
       case "marked":
@@ -17,37 +21,29 @@ export default Ember.Component.extend({
         return orderitem.get('count') - orderitem.get('countMarked') - orderitem.get('countPaid');
     }
   }),
-  sumTotal: Ember.computed('computedCount', function(){
+  sumTotal: Ember.computed('computedCount', function() {
     return this.get('computedCount') * this.get('orderitem.price');
   }),
-  sumMarked: Ember.computed('orderitem.countMarked', function(){
+  sumMarked: Ember.computed('orderitem.countMarked', function() {
     return this.get('orderitem.countMarked') * this.get('orderitem.price');
   }),
   incrementMarked() {
     const orderitem = this.get('orderitem');
-    if (this.get('type') === "open" && orderitem.get('countMarked') < orderitem.get('count') - orderitem.get('countPaid')){
+    if (this.get('type') === "open" && orderitem.get('countMarked') < orderitem.get('count') - orderitem.get('countPaid')) {
       orderitem.incrementProperty('countMarked');
     }
   },
   decrementMarked() {
     const orderitem = this.get('orderitem');
-    if(orderitem.get('countMarked') > 0) {
+    if (orderitem.get('countMarked') > 0) {
       orderitem.decrementProperty('countMarked');
     }
   },
-/*  click() {
-    const orderitem = this.get('orderitem');
-    if (this.get('type') === "open" && orderitem.get('countMarked') < orderitem.get('count') - orderitem.get('countPaid')){
-      orderitem.incrementProperty('countMarked');
-    } else if (this.get('type') === "marked" && orderitem.get('countMarked') > 0) {
-      orderitem.decrementProperty('countMarked');
-    }
-  },*/
   actions: {
-    incrementMarked(){
+    incrementMarked() {
       this.get('incrementMarked')();
     },
-    decrementMarked(){
+    decrementMarked() {
       this.get('incrementMarked')();
     }
   }
