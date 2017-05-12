11d29c1a9aa75e7e93126bdc23258ed7d7075f55
Unknown <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-05-12 13:15:54 +0200

    feat: add mark and unmark on desktop version

--- order/app/components/pay-detail/main-view.js
+++ order/app/components/pay-detail/main-view.js
@@ -8,26 +8,26 @@ export default Ember.Component.extend(RecognizerMixin, {
   classNames: ['pay-detail', 'screen'],
   recognizers: 'swipe',
   connection: true,
-  paidOrderitems: Ember.computed.filter('order.orderitems.@each.countPaid', function (orderitem) {
+  paidOrderitems: Ember.computed.filter('order.orderitems.@each.countPaid', function(orderitem) {
     if (orderitem.get('countPaid') > 0) return true;
     return false;
   }),
-  markedOrderitems: Ember.computed.filter('order.orderitems.@each.countMarked', function (orderitem) {
+  markedOrderitems: Ember.computed.filter('order.orderitems.@each.countMarked', function(orderitem) {
     if (orderitem.get('countMarked') > 0) return true;
     return false;
   }),
-  orderitems: Ember.computed.filter('order.orderitems.@each.{countMarked,countPaid}', function (orderitem) {
+  orderitems: Ember.computed.filter('order.orderitems.@each.{countMarked,countPaid}', function(orderitem) {
     if (orderitem.get('countPaid') < orderitem.get('count')) return true;
     return false;
   }),
-  markedAmount: Ember.computed('markedOrderitems', function () {
+  markedAmount: Ember.computed('markedOrderitems', function() {
     const orderitems = this.get('markedOrderitems');
     return orderitems.reduce((sum, orderitem) => sum + orderitem.get('price') * orderitem.get('countMarked'), 0);
   }),
-  openAmount: Ember.computed('order', 'order.openAmount', function () {
+  openAmount: Ember.computed('order', 'order.openAmount', function() {
     return this.get('order.openAmount');
   }),
-  open: Ember.computed('openAmount', function () {
+  open: Ember.computed('openAmount', function() {
     return this.get('openAmount') > 0;
   }),
   forFree: false,
@@ -42,7 +42,7 @@ export default Ember.Component.extend(RecognizerMixin, {
     this.get('pageTransitions').toScreen({ screen: 'pay-main', from: 'left' });
   },
   actions: {
-    setActualOrder(table){
+    setActualOrder(table) {
       this.set('order', table);
     },
     goToOrderMain() {
@@ -63,18 +63,29 @@ export default Ember.Component.extend(RecognizerMixin, {
       const orders = this.get('order.type') === 'table' ? this.get('order.orders') : [this.get('order')];
       this.get('connection') ? this.saveOrdersAPI(orders) : this.saveOrdersOffline(orders);
     },
+    toggleMarkAll() {
+      if (this.get('markedOrderitems').length != this.get('orderitems').length) {
+        this.get('orderitems').forEach(orderitem => {
+          orderitem.set('countMarked', orderitem.get('count'));
+        });
+      } else {
+        this.get('orderitems').forEach(orderitem => {
+          orderitem.set('countMarked', 0);
+        });
+      }
+    },
     printBill() {
       this.get('printBill')(this.get('order').id);
     }
   },
-  payAllOrderitems(){
+  payAllOrderitems() {
     this.get('order.orderitems').forEach(orderitem => {
       orderitem.set('countPaid', orderitem.get('count'));
       if (this.get('forFree')) orderitem.set('countFree', orderitem.get('count'));
       orderitem.set('countMarked', 0);
     });
   },
-  payMarkedOrderitems(){
+  payMarkedOrderitems() {
     this.get('markedOrderitems').forEach(orderitem => {
       orderitem.set('countPaid', orderitem.get('countPaid') + orderitem.get('countMarked'));
 
@@ -82,14 +93,14 @@ export default Ember.Component.extend(RecognizerMixin, {
       orderitem.set('countMarked', 0);
     });
   },
-  orderPromises(orders){
+  orderPromises(orders) {
     return orders.map(order => {
       order.set('isPaid', true);
       order.set('totalAmount', 0);
       return order.save();
     });
   },
-  saveOrdersOffline(orders){
+  saveOrdersOffline(orders) {
     orders.forEach(order => {
       order.set('totalAmount', order.get('openAmount'));
       const serializedOrder = order.serialize();
@@ -99,7 +110,7 @@ export default Ember.Component.extend(RecognizerMixin, {
       this.get('modal').closeModal();
     });
   },
-  saveOrdersAPI(orders){
+  saveOrdersAPI(orders) {
     Promise.all(this.orderPromises(orders)).then(() => {
       this.set('forFree', false);
       this.get('modal').closeModal();
