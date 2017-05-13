022babea1a7470de77cd15a4a530981a86c862b1
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-13 17:03:29 +0200

    add: temporary props and toggle button action

--- dashboard/app/components/single-event.js
+++ dashboard/app/components/single-event.js
@@ -2,6 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     tagName: 'li',
-    startDate: new Date(),
-    endDate: new Date()
+    todaysDate: new Date(),
+    name: 'Donutfest',
+    beginDate: new Date(),
+    endDate: new Date(),
+    customTables: true,
+    instantPay: false,
+    showItemPrice: false,
+    waiterAccess: false,
+    receiptPrinter: 'Drucker',
+    eventName: 'Eigentlich Kuchenfest',
+    expiresTime: 72,
+    actions: {
+        toggleButton(prop, value) {
+            var result = !value;
+            this.set(prop, result);
+        }
+    }
 });
