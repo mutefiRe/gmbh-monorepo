ccaf78693443e0d010018ac0f740b787d6318bbe
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-15 17:13:19 +0200

    fix indentation

--- dashboard/app/components/single-event.js
+++ dashboard/app/components/single-event.js
@@ -1,22 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    tagName: 'li',
-    todaysDate: new Date(),
-    name: 'Donutfest',
-    beginDate: new Date(),
-    endDate: new Date(),
-    customTables: true,
-    instantPay: false,
-    showItemPrice: false,
-    waiterAccess: false,
-    receiptPrinter: 'Drucker',
-    eventName: 'Eigentlich Kuchenfest',
-    expiresTime: 72,
-    actions: {
-        toggleButton(prop, value) {
-            var result = !value;
-            this.set(prop, result);
-        }
+  tagName: 'li',
+  todaysDate: new Date(),
+  name: 'Donutfest',
+  beginDate: new Date(),
+  endDate: new Date(),
+  customTables: true,
+  instantPay: false,
+  showItemPrice: false,
+  waiterAccess: false,
+  receiptPrinter: 'Drucker',
+  eventName: 'Eigentlich Kuchenfest',
+  expiresTime: 72,
+  actions: {
+    toggleButton(prop, value) {
+      const result = !value;
+      this.set(prop, result);
     }
+  }
 });
--- dashboard/app/routes/events.js
+++ dashboard/app/routes/events.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model() {
-        return Ember.RSVP.hash({
-            event: this.store.findAll('setting'),
-            printers: this.store.findAll('printer')
-        });
-    }
+  model() {
+    return Ember.RSVP.hash({
+      event: this.store.findAll('setting'),
+      printers: this.store.findAll('printer')
+    });
+  }
 });
