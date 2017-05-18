e1e7cbef981ed5bb186dc53cbc2d03e78b8861bc
Konrad Kleeberger <git@konrad.online>
2017-05-18 12:55:01 +0200

    feat: update to dynamic data for products route

--- dashboard/app/components/single-product.js
+++ dashboard/app/components/single-product.js
@@ -2,10 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   editable: Ember.inject.service(),
+  store: Ember.inject.service(),
   tagName: 'li',
   actions: {
     toggleEditable() {
       this.get('editable').toggle(this);
+    },
+    changeRelation(product, event) {
+      const category = this.get('store').peekRecord('category', event.target.value)
+      product.set('category', category);
+    },
+    updateProduct(product) {
+      product.save();
+    },
+    destroyProduct(product) {
+      product.destroyRecord();
     }
   }
 });
