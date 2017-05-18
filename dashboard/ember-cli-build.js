c0c319f8d408a32bccfc1d8a6c9002191f72084c
Konrad Kleeberger <git@konrad.online>
2017-05-18 12:57:40 +0200

    fix: tests did not use ES-2015 polyfills

--- dashboard/ember-cli-build.js
+++ dashboard/ember-cli-build.js
@@ -1,10 +1,11 @@
-/*jshint node:true*/
 /* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
+  const app = new EmberApp(defaults, {
+    'ember-cli-babel': {
+      includePolyfill: true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
