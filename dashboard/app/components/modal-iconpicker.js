00588105ab8c1e75a10bb5c8ae9d8e7e11035179
Konrad Kleeberger <git@konrad.online>
2017-05-18 12:55:53 +0200

    feat: update to dynamic form data for route categories calculate icon data based on css classes get color codes from json file switch to ember-modals for display of data

--- dashboard/app/components/modal-iconpicker.js
+++ dashboard/app/components/modal-iconpicker.js
@@ -1,16 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  modal: Ember.inject.service(),
   tagName: 'div',
-  classNames: ['modal'],
   actions: {
-    hideModal() {
-      this.get('modal').hideModal(this);
-    },
-    changeIcon(type, name) {
-      this.sendAction('action', type, name);
-      this.send('hideModal');
+    selectAndClose(icon) {
+      Ember.set(this.category, 'icon', icon);
+      this.set('switch', false);
     }
   }
 });
