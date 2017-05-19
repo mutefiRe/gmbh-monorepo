467b3669e10922e409916a5e8046505159f43f4b
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-19 10:16:27 +0200

    add: single area component and styles

---
+++ dashboard/app/components/single-area.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  editable: Ember.inject.service(),
+  tagName: 'li',
+  actions: {
+    toggleEditable() {
+      this.get('editable').toggle(this);
+    }
+  }
+});
