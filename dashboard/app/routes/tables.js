f41b07ada4efcb8e7e080e06331a5e062d9acdd9
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-19 18:07:20 +0200

    add: single table component

--- dashboard/app/routes/tables.js
+++ dashboard/app/routes/tables.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      tables: this.store.findAll('table'),
+      areas: this.store.findAll('area')
+    });
+  }
 });
