2a9b538f0f02432bdf6df6e2a56ff02a813301e3
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-20 21:49:21 +0200

    add: save, delete and change functions

--- dashboard/app/components/single-table.js
+++ dashboard/app/components/single-table.js
@@ -2,13 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   editable: Ember.inject.service(),
+  store: Ember.inject.service(),
   tagName: 'li',
   actions: {
     toggleEditable() {
       this.get('editable').toggle(this);
     },
-    changeRelation() {
-      console.log('gg');
+    changeRelation(table, event) {
+      const area = this.get('store').peekRecord('area', event.target.value)
+      table.set('area', area);
+    },
+    updateTable(table) {
+      table.save();
+    },
+    destroyTable(table) {
+      table.destroyRecord();
     }
+
   }
 });
