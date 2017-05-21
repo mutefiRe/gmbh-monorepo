5084b2bb953417a7bccb6cb8bc993d8091b5d614
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-21 10:12:04 +0200

    fix: area change only on save

--- dashboard/app/components/single-table.js
+++ dashboard/app/components/single-table.js
@@ -4,15 +4,17 @@ export default Ember.Component.extend({
   editable: Ember.inject.service(),
   store: Ember.inject.service(),
   tagName: 'li',
+  areaToSet: '',
   actions: {
     toggleEditable() {
       this.get('editable').toggle(this);
     },
     changeRelation(table, event) {
-      const area = this.get('store').peekRecord('area', event.target.value)
-      table.set('area', area);
+      const area = this.get('store').peekRecord('area', event.target.value);
+      this.set('areaToSet', area);
     },
     updateTable(table) {
+      table.set('area', this.get('areaToSet'));
       table.save();
     },
     destroyTable(table) {
