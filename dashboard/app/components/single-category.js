0273b7adef6e6caa681a061a5ea29cedb8bf4d76
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-23 14:15:58 +0200

    add: disable enable button for categories

--- dashboard/app/components/single-category.js
+++ dashboard/app/components/single-category.js
@@ -4,10 +4,14 @@ export default Ember.Component.extend(Ember.Evented, {
   isShowingIcons: false,
   isShowingColors: false,
   editable: Ember.inject.service(),
+  enable: Ember.inject.service(),
   tagName: 'li',
   actions: {
     toggleEditable() {
       this.get('editable').toggle({ component: this, record: this.get('category') });
+    },
+    toggleButton(category, prop, value) {
+      this.get('enable').toggleBtn(category, prop, value);
     },
     updateCategory(category) {
       category.save().then(() => {
