efb3a3ad99900006ee161d2c8e6409c7f234bdae
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-23 22:27:55 +0200

    add: disable enable button for tables

--- dashboard/app/components/single-table.js
+++ dashboard/app/components/single-table.js
@@ -2,12 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   editable: Ember.inject.service(),
+  enable: Ember.inject.service(),
   store: Ember.inject.service(),
   tagName: 'li',
   areaToSet: '',
+  isEnabled: Ember.computed('table.enabled', 'table.area.enabled', function() {
+    return this.get('table.enabled') && this.get('table.area.enabled');
+  }),
   actions: {
     toggleEditable() {
       this.get('editable').toggle({ component: this, record: this.get('table') });
+    },
+    toggleButton(product, prop, value) {
+      this.get('enable').toggleBtn(product, prop, value);
     },
     changeRelation(table, event) {
       const area = this.get('store').peekRecord('area', event.target.value);
