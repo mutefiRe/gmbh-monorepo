081d0a3181190cc1f79ea50e4b703e8dc3182511
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-23 22:08:21 +0200

    add: disable enable button for areas

--- dashboard/app/components/single-area.js
+++ dashboard/app/components/single-area.js
@@ -2,10 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   editable: Ember.inject.service(),
+  enable: Ember.inject.service(),
   tagName: 'li',
   actions: {
     toggleEditable() {
       this.get('editable').toggle({ component: this, record: this.get('area') });
+    },
+    toggleButton(product, prop, value) {
+      this.get('enable').toggleBtn(product, prop, value);
     },
     updateArea(area) {
       area.save().then(() => {
