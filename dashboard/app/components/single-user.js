9a4f527eb7c4bff64a84df42bcc8f79db029b87c
Unknown <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-05-24 17:41:24 +0200

    feat: remove service from mobile controlbar in users

--- dashboard/app/components/single-user.js
+++ dashboard/app/components/single-user.js
@@ -10,16 +10,20 @@ export default Ember.Component.extend({
     updateUser(user) {
       user.save().then(() => {
         this.send('toggleEditable');
-
-        // notify user (success)
         this.get('notifications').success(this.get('i18n').t('notifications.user.update.success'));
       }).catch(() => {
-        // notify user (failure)
         this.get('notifications').error(this.get('i18n').t('notifications.user.update.error'));
       });
     },
     toggleEditable() {
       this.toggleProperty('isOpen');
+      if (this.get('isOpen')) {
+        $('body').addClass('noscroll');
+        this.set('currentSelectedUser', { component: this, record: this.get('user') });
+      } else {
+        $('body').removeClass('noscroll');
+        this.set('currentSelectedUser', null);
+      }
     }
   }
 });
