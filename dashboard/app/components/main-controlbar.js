6cc205c2a2e48d5a9f8455aa2600447947c21515
Unknown <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-05-24 17:41:24 +0200

    feat: remove service from mobile controlbar in users

--- dashboard/app/components/main-controlbar.js
+++ dashboard/app/components/main-controlbar.js
@@ -1,12 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  editable: Ember.inject.service(),
+  notifications: Ember.inject.service('notification-messages'),
+  i18n: Ember.inject.service(),
   tagName: 'div',
   classNames: ['addentrybar'],
   actions: {
     saveRecord() {
-      this.get('editable').saveRecord();
+      const currentRecord = this.get('currentSelectedRecord');
+      currentRecord.record.save().then(() => {
+        currentRecord.component.set('isOpen', false);
+        this.get('notifications').success(this.get('i18n').t('notifications.record.save.success'));
+      }).catch(() => {
+        this.get('notifications').error(this.get('i18n').t('notifications.record.save.error'));
+      });
     }
   }
 
