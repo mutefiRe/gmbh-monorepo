26cfc355c6b79b873f1e073a59a1eb5d7551ceb1
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2017-05-27 17:35:50 +0200

    feat: add i18n for password modal

--- dashboard/app/components/modal-changepassword.js
+++ dashboard/app/components/modal-changepassword.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
+  i18n: Ember.inject.service(),
   actions: {
     save(password) {
       this.get('user').set('password', password);
