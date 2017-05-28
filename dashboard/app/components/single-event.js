84a55d3e90cbcace5a3606f0ffc05040b31d5a53
Konrad Kleeberger <git@konrad.online>
2017-05-28 22:05:41 +0200

    fix: add relation to printer model

--- dashboard/app/components/single-event.js
+++ dashboard/app/components/single-event.js
@@ -4,7 +4,8 @@ export default Ember.Component.extend({
   tagName: 'li',
   todaysDate: new Date(),
   notifications: Ember.inject.service('notification-messages'),
-  i18n: Ember.inject.service(),
+  i18n:          Ember.inject.service(),
+  store:         Ember.inject.service(),
   init() {
     const singleEvent = this.get('events.firstObject');
     if (singleEvent === undefined) {
@@ -24,10 +25,16 @@ export default Ember.Component.extend({
     toggleButton(prop) {
       this.get('event').toggleProperty(prop);
     },
+    changeRelation(event, e) {
+      const printer = this.get('store').peekRecord('printer', e.target.value);
+      event.set('receiptPrinter', printer);
+    },
     updateEvent(event) {
       event.save().then(() => {
+        // notify user (success)
         this.get('notifications').success(this.get('i18n').t('notifications.event.update.success'));
       }).catch(() => {
+        // notify user (failure)
         this.get('notifications').error(this.get('i18n').t('notifications.event.update.error'));
       });
     }
