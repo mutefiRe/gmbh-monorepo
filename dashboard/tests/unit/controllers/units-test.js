c539d80fd7ab2653c8d7c8ced10bcdd7bced7866
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2017-05-28 21:48:56 +0200

    test: fix renamed unit controller test

--- dashboard/tests/unit/controllers/units-test.js
+++ dashboard/tests/unit/controllers/units-test.js
@@ -2,15 +2,15 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupTest } from 'ember-mocha';
 
-describe('Unit | Controller | unit', function() {
-  setupTest('controller:unit', {
+describe('Units | Controller | units', function() {
+  setupTest('controller:units', {
     // Specify the other units that are required for this test.
     // needs: ['controller:foo']
   });
 
   // Replace this with your real tests.
   it('exists', function() {
-    let controller = this.subject();
+    const controller = this.subject();
     expect(controller).to.be.ok;
   });
 });
