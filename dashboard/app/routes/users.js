8070a77ffd485de1e6f1f38c2e9cd8b61fb2c8dc
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2017-05-29 15:27:50 +0200

    feat: add printer selecting for user

--- dashboard/app/routes/users.js
+++ dashboard/app/routes/users.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('user', { reload: true });
+    return Ember.RSVP.hash({
+      users: this.get('store').findAll('user', { reload: true }),
+      printers: this.get('store').findAll('printer', { reload: true })
+    });
   }
 });
